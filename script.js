20d85f6d264d94e78d6a5352467e89a5f7ea843a
whosee21 <kidacoe21@yahoo.com>
2025-01-20 01:43:57 +0800

    Added button for random color generator

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const div = document.getElementById("container");
 const clear = document.getElementById("clear");
 const newgrid = document.getElementById("newgrid");
+const random = document.getElementById("random");
+
 const GRIDSIZE = 550;
 
 let size = 16;
+let isRandom = false;
 
-function gridCreate(size){
+function gridCreate(size, isRandom){
     div.innerHTML = '';
     div.style.width = `${GRIDSIZE}px`;
     div.style.height = `${GRIDSIZE}px`;
@@ -15,18 +18,32 @@ function gridCreate(size){
         box.style.backgroundColor = "white";
         box.classList.add("box");
 
-        box.addEventListener('mouseenter', () => {
-            box.style.backgroundColor = "black";
-        });
+        if (isRandom === true) {
+            box.addEventListener('mouseenter', () => {
+                box.style.backgroundColor = `rgb(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255})`;
+            });
+        } else if (isRandom === false) {
+            box.addEventListener('mouseenter', () => {
+                box.style.backgroundColor = "black";
+            });
+        }
 
         div.appendChild(box);
     }
 }
 
+random.addEventListener('click', () => {
+    if (isRandom === false) {
+        isRandom = true;
+    } else if (isRandom === true) {
+        isRandom = false;
+    }
+    gridCreate(size, isRandom)
+});
+
 newgrid.addEventListener('click', () => {
     size = prompt("Enter new grid size between 1 - 100", 16);
     if (size !== null && size !== ''){
-
         if (size == '') {
             size = 16;
         } else if (size > 100) {
@@ -37,23 +54,25 @@ newgrid.addEventListener('click', () => {
     } else {
         size = 16;
         alert("Cancelled! Grid returned to default size of 16x16.");
-        gridCreate(size);
+        gridCreate(size, isRandom);
     }
     size = parseInt(size);
-    gridCreate(size);
+    gridCreate(size, isRandom);
 });
-
 
 clear.addEventListener('click', () => {
-    gridClear();
-});
-
-function gridClear(){
+    // gridClear();
     const boxes = document.getElementsByClassName("box");
     for (let i = 0; i < boxes.length; i++) {
         boxes[i].style.backgroundColor = "white";
     }
-}
+});
 
+// function gridClear(){
+//     const boxes = document.getElementsByClassName("box");
+//     for (let i = 0; i < boxes.length; i++) {
+//         boxes[i].style.backgroundColor = "white";
+//     }
+// }
 
-gridCreate(size);
+gridCreate(size, isRandom);
