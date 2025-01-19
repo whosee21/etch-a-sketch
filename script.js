c7c8463a24e840dea999978053d6db3171ab1e57
whosee21 <kidacoe21@yahoo.com>
2025-01-19 18:22:41 +0800

    added sidebar container and title Added mouse hover event

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 const div = document.getElementById("container");
-// div.style.width = `600px`;
 const GRIDSIZE = 550;
 
 let size = 16;
 
-div.style.width = `${GRIDSIZE}px`;
-div.style.height = `${GRIDSIZE}px`;
-for (let i = 0; i < (size * size); i++) {
-    const box = document.createElement("div");
-    box.style.height = `${GRIDSIZE/size}px`;
-    box.style.width = `${GRIDSIZE/size}px`;
-    box.style.flex = `0 0 ${100/size}%`;
-    box.classList.add("box");
-    div.appendChild(box);
+function gridCreate(){
+    div.style.width = `${GRIDSIZE}px`;
+    div.style.height = `${GRIDSIZE}px`;
+    for (let i = 0; i < (size * size); i++) {
+        const box = document.createElement("div");
+        box.style.flex = `0 0 ${100/size}%`;
+        box.style.backgroundColor = "white";
+        box.classList.add("box");
+
+        box.addEventListener('mouseenter', () => {
+            box.style.backgroundColor = "black";
+        });
+
+        div.appendChild(box);
+    }
 }
+
+gridCreate();
