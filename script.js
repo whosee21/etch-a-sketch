39b0a37dbf76c545d1b0e83a3c96f0a66d4a6239
whosee21 <kidacoe21@yahoo.com>
2025-01-19 22:37:56 +0800

    Added clear function Added newgrid function that takes value from user via prompt

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const div = document.getElementById("container");
+const clear = document.getElementById("clear");
+const newgrid = document.getElementById("newgrid");
 const GRIDSIZE = 550;
 
 let size = 16;
 
-function gridCreate(){
+function gridCreate(size){
+    div.innerHTML = '';
     div.style.width = `${GRIDSIZE}px`;
     div.style.height = `${GRIDSIZE}px`;
     for (let i = 0; i < (size * size); i++) {
@@ -20,4 +23,37 @@ function gridCreate(){
     }
 }
 
-gridCreate();
+newgrid.addEventListener('click', () => {
+    size = prompt("Enter new grid size between 1 - 100", 16);
+    if (size !== null && size !== ''){
+
+        if (size == '') {
+            size = 16;
+        } else if (size > 100) {
+            size = 100;
+        } else if (size < 1) {
+            size = 1;
+        } 
+    } else {
+        size = 16;
+        alert("Cancelled! Grid returned to default size of 16x16.");
+        gridCreate(size);
+    }
+    size = parseInt(size);
+    gridCreate(size);
+});
+
+
+clear.addEventListener('click', () => {
+    gridClear();
+});
+
+function gridClear(){
+    const boxes = document.getElementsByClassName("box");
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].style.backgroundColor = "white";
+    }
+}
+
+
+gridCreate(size);
