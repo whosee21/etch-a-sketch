07dde5fa17e5af67b95543e6800c236a6fc01310
whosee21 <kidacoe21@yahoo.com>
2025-01-19 17:36:40 +0800

    Rewritten css styles Reworked html structure Added default 16x16 gridbox using flexbox

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 const div = document.getElementById("container");
+// div.style.width = `600px`;
+const GRIDSIZE = 550;
 
+let size = 16;
+
+div.style.width = `${GRIDSIZE}px`;
+div.style.height = `${GRIDSIZE}px`;
+for (let i = 0; i < (size * size); i++) {
+    const box = document.createElement("div");
+    box.style.height = `${GRIDSIZE/size}px`;
+    box.style.width = `${GRIDSIZE/size}px`;
+    box.style.flex = `0 0 ${100/size}%`;
+    box.classList.add("box");
+    div.appendChild(box);
+}
